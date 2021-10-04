5d96f46af17f5b25976fc074a5167304d52c5956
Wenyi Liu <wenyiliu95@gmail.com>
2021-10-03 23:23:22 -0400

    Week2

--- Week2/read_lines_array2.cpp
+++ Week2/read_lines_array2.cpp
@@ -1,9 +1,9 @@
 #include <iostream>  // std::cout, std::endl
 #include <exception>  // std::exception
 #include <fstream>  // std::ifstream
+#include <string>  // std::string
 #include <utility>  // std::pair, std::make_pair
-#include <string>
-#include <vector>// std::string
+#include <vector>  // std::vector
 
 using std::cout;
 using std::endl;
@@ -18,7 +18,7 @@ using std::vector;
 // It's good to subclass on an existing exception type when you write a library
 // so people using your code can choose to catch the specific exception type.
 class read_lines_exception : public exception {
-  string what_str;
+  stringwhat_str;
 public:
   read_lines_exception(const string& what_str) : what_str(what_str) {}
   const char* what() const noexcept override {
@@ -38,8 +38,8 @@ pair<string*, int> read_lines_array(const string& filepath) {
   // everything to the new array, then delete[] the old array.
   //
   // No matter how big we make the original array, it might have to get bigger,
-  // so let's just start with a capacity (number of lines we can fit) of 10.
-  int lines_capacity = 10;
+  // so let's just start with a capacity (number of lines we can fit) of 1.
+  int lines_capacity = 1;
   // We also need to keep track of how many lines we have so far.
   int lines_size = 0;
   string *lines = new string[lines_capacity];
@@ -75,6 +75,136 @@ pair<string*, int> read_lines_array(const string& filepath) {
   return make_pair(lines, lines_size);
 }
 
+
+class StringVector {
+  // We need to keep track of how much room is in the array (capacity)
+  // and the number of elements in the array (size_).
+  int capacity, size_;
+  string *strs;
+
+public:
+  StringVector() : capacity(1), size_(0) {
+    strs = new string[capacity];
+  }
+
+  // Don't forget to delete dynamically allocated arrays when we're done using
+  // them! Alternatively, use an std::unique_ptr.
+  ~StringVector() {
+    delete[] strs;
+  }
+
+  void push_back(const string& str) {
+    if (size_ >= capacity) {
+      const int new_capacity = 2 * capacity;
+      string *new_strs = new string[new_capacity];
+      for (int i = 0; i < size_; ++i) {
+        new_strs[i] = strs[i];
+      }
+      delete[] strs;
+      strs = new_strs;
+      capacity = new_capacity;
+    }
+    strs[size_] = str;
+    ++size_;
+  }
+
+  string& operator[](int index) {
+    return strs[index];
+  }
+
+  int size() {
+    return size_;
+  }
+
+  string* begin() {
+    return strs;  // same as &strs[0]
+  }
+
+  string* end() {
+    return strs + size_;  // same as &strs[size_]
+  }
+};
+
+
+StringVector read_lines_string_vector(const string& filepath) {
+  ifstream f(filepath);
+  StringVector lines;
+  if (f.fail()) {
+    throw read_lines_exception("Could not open file " + filepath);
+  }
+  for(string line; getline(f, line); ) {
+    lines.push_back(line);
+  }
+  if (f.bad()) {
+    throw read_lines_exception("Failed to read all lines in " + filepath);
+  }
+  return lines;
+}
+
+
+template<typename T>
+class TemplateVector {
+  int capacity, size_;
+  T *values;
+
+public:
+  TemplateVector() : capacity(1), size_(0) {
+    values = new T[capacity];
+  }
+
+  ~TemplateVector() {
+    delete[] values;
+  }
+
+  void push_back(const T& value) {
+    if (size_ >= capacity) {
+      const int new_capacity = 2 * capacity;
+      T *new_values = new T[new_capacity];
+      for (int i = 0; i < size_; ++i) {
+        new_values[i] = values[i];
+      }
+      delete[] values;
+      values = new_values;
+      capacity = new_capacity;
+    }
+    values[size_] = value;
+    ++size_;
+  }
+
+  T& operator[](int index) {
+    return values[index];
+  }
+
+  int size() {
+    return size_;
+  }
+
+  T* begin() {
+    return values;  // same as &values[0]
+  }
+
+  T* end() {
+    return values + size_;  // same as &values[size_]
+  }
+};
+
+
+TemplateVector<string> read_lines_template_vector(const string& filepath) {
+  ifstream f(filepath);
+  TemplateVector<string> lines;
+  if (f.fail()) {
+    throw read_lines_exception("Could not open file " + filepath);
+  }
+  for(string line; getline(f, line); ) {
+    lines.push_back(line);
+  }
+  if (f.bad()) {
+    throw read_lines_exception("Failed to read all lines in " + filepath);
+  }
+  return lines;
+}
+
+
 vector<string> read_lines_std_vector(const string& filepath) {
   ifstream f(filepath);
   vector<string> lines;
@@ -90,6 +220,7 @@ vector<string> read_lines_std_vector(const string& filepath) {
   return lines;
 }
 
+
 int main(int argc, const char *argv[]) {
   string filepath = argc > 1 ?  argv[1] : "truth.txt";
 
@@ -101,14 +232,23 @@ int main(int argc, const char *argv[]) {
     cout << lines[i] << endl;
   }
   cout << endl;
-  // Don't forget to delete[] dynamically allocated arrays when you're done!
   delete[] lines;
 
   cout << "read_lines_string_vector:" << endl;
-  for (const string& line : read_lines_std_vector(filepath)) {
+  for (const string& line : read_lines_string_vector(filepath)) {
     cout << line << endl;
   }
   cout << endl;
 
+  cout << "read_lines_template_vector:" << endl;
+  for (const string& line : read_lines_template_vector(filepath)) {
+    cout << line << endl;
+  }
+  cout << endl;
+
+  cout << "read_lines_std_vector:" << endl;
+  for (const string& line : read_lines_std_vector(filepath)) {
+    cout << line << endl;
+  }
   return 0;
 }
